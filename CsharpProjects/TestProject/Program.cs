1d5ec1c0717dd8b3531044deb408b2593b724c94
longpt271 <longpt271@gmail.com>
2023-11-26 22:04:36 +0700

    roll dice

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello C#!");
+
+// Gọi các loại phương thức khác nhau trong Thư viện lớp .NET
+// VD: xúc xắc random ra số ngẫu nhiên
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
