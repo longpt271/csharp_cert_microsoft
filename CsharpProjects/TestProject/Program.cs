8b2330cb02e16692e013b433b4c9d01e59e22110
longpt271 <longpt271@gmail.com>
2023-11-26 22:15:13 +0700

    Math class methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -15,3 +15,10 @@ int roll3 = dice.Next(50, 101);
 Console.WriteLine($"First roll: {roll1}");
 Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}");
+
+// Math class methods
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue,secondValue);
+
+Console.WriteLine(largerValue);
